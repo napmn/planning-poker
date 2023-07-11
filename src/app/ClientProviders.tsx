2aba9ac9d9f5217b74cd87546e286fb6577b1fcd
napmn <lukass135@gmail.com>
2023-07-11 21:03:13 +0200

    Added react-query

--- src/app/ClientProviders.tsx
+++ src/app/ClientProviders.tsx
@@ -1,7 +1,23 @@
 'use client'
 
 import { NextUIProvider } from '@nextui-org/system'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-export const ClientProviders: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return <NextUIProvider>{children}</NextUIProvider>
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 10000 // 10s
+    }
+  }
+})
+
+export const ClientProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>{children}</NextUIProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
