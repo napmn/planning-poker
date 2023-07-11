0d661d75e0cae6e0fabdb611bba68230cda76ac0
napmn <lukass135@gmail.com>
2023-07-11 21:58:52 +0200

    Removed semicolons

--- src/app/api/planning-room/route.ts
+++ src/app/api/planning-room/route.ts
@@ -1,13 +1,13 @@
-import { NextResponse } from 'next/server';
-import { v4 as uuidv4 } from 'uuid';
+import { NextResponse } from 'next/server'
+import { v4 as uuidv4 } from 'uuid'
 
-import { db } from '@/lib/db';
-import { RoomTable } from '@/lib/db/schema';
+import { db } from '@/lib/db'
+import { RoomTable } from '@/lib/db/schema'
 
 export async function POST() {
   // TODO implement rate limit
   const room = { token: uuidv4() }
   await db.insert(RoomTable).values(room)
 
-  return NextResponse.json({ token: room.token });
+  return NextResponse.json({ token: room.token })
 }
